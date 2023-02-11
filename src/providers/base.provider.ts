722b97be92f2cad5a055aa322c855df5b5de61c7
Kevin Rüter <kevin@rueter.email>
2023-02-11 13:16:57 +0100

    use base provider for fetching tournaments

--- src/providers/base.provider.ts
+++ src/providers/base.provider.ts
@@ -1,13 +1,83 @@
+import axios from 'axios';
+import { parse as parseHTML } from 'node-html-parser';
+
 import { ITournament } from "../interfaces/tournament.interface";
 
 export abstract class BaseProvider {
+  protected baseUrl: string;
+  protected federation: string;
+
+  /**
+   * Constructor of the base provider.
+   *
+   * @param baseUrl
+   */
+  constructor(baseUrl: string, federation: string) {
+    this.baseUrl = baseUrl;
+    this.federation = federation;
+  }
+
   /**
    *
+   * @param federation
    * @param circuit
    * @param date
    * @param areas
    */
-  public abstract parseTournaments(circuit: string, date: string, areas: string[]): Promise<ITournament[]>;
+  public async parseTournaments(
+    circuit: string,
+    date?: string,
+    areas?: string[]
+  ): Promise<ITournament[]> {
+    const now = new Date();
+
+    const currentYear = now.getFullYear();
+    const currentMonth = now.getMonth() + 1;
+
+    const requestDate = date || `${currentYear}-${currentMonth}-01`;
+
+    const response = await axios.get(this.baseUrl, {
+      params: { federation: this.federation, circuit, date: requestDate }
+    });
+
+    const parsedData = parseHTML(response.data);
+
+    const resultSet = parsedData.querySelector('.result-set');
+
+    const resultRows = resultSet?.getElementsByTagName('tr');
+
+    if (!resultRows) {
+      console.error('No result rows found');
+      return [];
+    }
+
+    let tournaments: ITournament[] = [];
+
+    // We skip the first row since this is the header row.
+    for (let i = 1; i < resultRows.length; i++) {
+      const resultCols = resultRows[i]?.getElementsByTagName('td');
+
+      if (!resultCols || resultCols.length < 5) {
+        continue;
+      }
+
+      const area = resultCols[4].innerText.trim();
+
+      if (areas && !areas.includes(area)) {
+        continue;
+      }
+
+      tournaments.push({
+        date: this.parseDate(resultCols[0].innerText.trim()),
+        participants: resultCols[2].innerText.trim(),
+        area
+      });
+    }
+
+    tournaments = tournaments.filter(tournament => tournament.date.getTime() >= now.getTime());
+
+    return tournaments;
+  }
 
   /**
    * Parse a date string to a date object.
--- src/providers/wttv.provider.ts
+++ src/providers/wttv.provider.ts
@@ -6,9 +6,13 @@ import { ITournament } from '../interfaces/tournament.interface';
 
 export class WTTVProvider extends BaseProvider {
   private static readonly BASE_URL = 'https://wttv.click-tt.de/cgi-bin/WebObjects/nuLigaTTDE.woa/wa/tournamentCalendar';
+  private static readonly FEDERATION = 'WTTV';
 
+  /**
+   * Constructor of the WTTV provider.
+   */
   constructor() {
-    super();
+    super(WTTVProvider.BASE_URL, WTTVProvider.FEDERATION);
   }
 
   /**
@@ -19,47 +23,9 @@ export class WTTVProvider extends BaseProvider {
    */
   public async parseTournaments(
     circuit = '2023_Turnierserie',
-    date = 'date=2023-01-01',
+    date?: string,
     areas = ['Bielefeld/Halle']
   ): Promise<ITournament[]> {
-    const response = await axios.get(WTTVProvider.BASE_URL, {
-      params: { federation: 'WTTV', circuit, date }
-    });
-
-    const parsedData = parseHTML(response.data);
-
-    const resultSet = parsedData.querySelector('.result-set');
-
-    const resultRows = resultSet?.getElementsByTagName('tr');
-
-    if (!resultRows) {
-      console.error('No result rows found');
-      return [];
-    }
-
-    const tournaments: ITournament[] = [];
-
-    // We skip the first row since this is the header row.
-    for (let i = 1; i < resultRows.length; i++) {
-      const resultCols = resultRows[i]?.getElementsByTagName('td');
-
-      if (!resultCols || resultCols.length < 5) {
-        continue;
-      }
-
-      const area = resultCols[4].innerText.trim();
-
-      if (!areas.includes(area)) {
-        continue;
-      }
-
-      tournaments.push({
-        date: this.parseDate(resultCols[0].innerText.trim()),
-        participants: resultCols[2].innerText.trim(),
-        area
-      });
-    }
-
-    return tournaments;
+    return super.parseTournaments(circuit, date, areas);
   }
 }
