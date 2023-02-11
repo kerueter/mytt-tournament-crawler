4c1122c5afbb64067fbed0512ff1a734518cf6d4
Kevin Rüter <kevin@rueter.email>
2023-02-11 14:23:19 +0100

    added some coloring to free spots

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,21 @@ async function run(): Promise<void> {
     totalTournaments.sort((a, b) => a.date.getTime() - b.date.getTime());
 
     for (const tournament of totalTournaments) {
+      let colorCode = 32;
+
+      const freeSpotsParts = tournament.freeSpots.split('/');
+      const freeSpots = Number(freeSpotsParts[0]);
+      const totalSpots = Number(freeSpotsParts[1]);
+
+      if (freeSpots === 0) {
+        colorCode = 31;
+      } else if (totalSpots > 0 && (freeSpots / totalSpots) < 0.25) {
+        colorCode = 32;
+      }
+
       console.log(`Datum: ${tournament.date.toLocaleString('de-DE', { dateStyle: 'full', timeStyle: 'short' })}`);
       console.log(`Organisator: ${tournament.organizer}`);
-      console.log(`Freie Plätze: ${tournament.freeSpots}`);
+      console.log(`\x1b[${colorCode}m%s\x1b[0m`, `Freie Plätze: ${tournament.freeSpots}`);
       console.log(`Warteliste: ${tournament.waitingList}`);
       console.log(`Area: ${tournament.area}`);
 
